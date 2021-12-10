b43d8f6178162a0b6ee5350df101873dae530304
inwananwar99 <inwananwar9b@gmail.com>
2021-12-10 10:36:02 +0700

    variable

---
+++ variable.dart
@@ -0,0 +1,16 @@
+void main() {
+  String firstName = 'Inwan Anwar';
+  print(firstName);
+  final lastName = 'Solihudin';
+  print(lastName);
+  // lastName = 'Lakek';
+  // print(lastName);
+  late var nilai = nama();
+  print('Asik');
+  print(nilai);
+}
+
+String nama() {
+  print('Hai, namaku Inwan');
+  return 'kEPOOOO';
+}
